f718cb4377a427f9e26bdbf138898a27d721da81
Nick Elsmore <nicholaselsmore@gmail.com>
2021-10-21 12:43:27 -0700

    cpu: integrate subtract instructions

--- src/cpu/test.rs
+++ src/cpu/test.rs
@@ -205,3 +205,189 @@ fn test_add_immediate_overflow() {
     assert_eq!(cpu.h, true);
     assert_eq!(cpu.cy, true);
 }
+
+// Verify decrementing registers
+#[test]
+fn test_all_decrements() {
+    let mut cpu = Cpu::new();
+    const INSTRUCTIONS_LEN: usize = 7;
+    let test_ram: [u8; INSTRUCTIONS_LEN] = [
+        Instruction::Dec(ArithmeticOperand::B).as_byte(),
+        Instruction::Dec(ArithmeticOperand::C).as_byte(),
+        Instruction::Dec(ArithmeticOperand::D).as_byte(),
+        Instruction::Dec(ArithmeticOperand::E).as_byte(),
+        Instruction::Dec(ArithmeticOperand::H).as_byte(),
+        Instruction::Dec(ArithmeticOperand::L).as_byte(),
+        Instruction::Dec(ArithmeticOperand::A).as_byte(),
+    ];
+
+    cpu.rf.iter_mut().for_each(|x| *x = 1);
+    cpu.load_test_ram(&test_ram);
+    for i in 0..test_ram.len() {
+        cpu.step();
+    }
+    assert_eq!(cpu.rf[ArithmeticOperand::B as usize], 0);
+    assert_eq!(cpu.rf[ArithmeticOperand::C as usize], 0);
+    assert_eq!(cpu.rf[ArithmeticOperand::D as usize], 0);
+    assert_eq!(cpu.rf[ArithmeticOperand::E as usize], 0);
+    assert_eq!(cpu.rf[ArithmeticOperand::H as usize], 0);
+    assert_eq!(cpu.rf[ArithmeticOperand::L as usize], 0);
+    assert_eq!(cpu.rf[ArithmeticOperand::A as usize], 0);
+}
+
+// Verify subtracting registers
+#[test]
+fn test_sub() {
+    let mut cpu = Cpu::new();
+    const INSTRUCTIONS_LEN: usize = 6;
+    let test_ram: [u8; INSTRUCTIONS_LEN] = [
+        Instruction::Sub(ArithmeticOperand::B).as_byte(),
+        Instruction::Sub(ArithmeticOperand::C).as_byte(),
+        Instruction::Sub(ArithmeticOperand::D).as_byte(),
+        Instruction::Sub(ArithmeticOperand::E).as_byte(),
+        Instruction::Sub(ArithmeticOperand::H).as_byte(),
+        Instruction::Sub(ArithmeticOperand::L).as_byte(),
+    ];
+
+    cpu.load_test_ram(&test_ram);
+    cpu.rf.iter_mut().for_each(|x| *x = 1);
+
+    for i in 0..test_ram.len() {
+        cpu.rf[ArithmeticOperand::A as usize] = 1;
+        cpu.step();
+
+        assert_eq!(cpu.z, true);
+        assert_eq!(cpu.n, true);
+        assert_eq!(cpu.h, false);
+        assert_eq!(cpu.cy, false);
+        assert_eq!(cpu.rf[ArithmeticOperand::A as usize], 0);
+    }
+}
+
+// Verify subtracting registers with overflow
+#[test]
+fn test_sub_overflow() {
+    let mut cpu = Cpu::new();
+    const INSTRUCTIONS_LEN: usize = 6;
+    let test_ram: [u8; INSTRUCTIONS_LEN] = [
+        Instruction::Sub(ArithmeticOperand::B).as_byte(),
+        Instruction::Sub(ArithmeticOperand::C).as_byte(),
+        Instruction::Sub(ArithmeticOperand::D).as_byte(),
+        Instruction::Sub(ArithmeticOperand::E).as_byte(),
+        Instruction::Sub(ArithmeticOperand::H).as_byte(),
+        Instruction::Sub(ArithmeticOperand::L).as_byte(),
+    ];
+
+    cpu.load_test_ram(&test_ram);
+    cpu.rf.iter_mut().for_each(|x| *x = 1);
+
+    for i in 0..test_ram.len() {
+        cpu.rf[ArithmeticOperand::A as usize] = 0;
+        cpu.step();
+
+        assert_eq!(cpu.z, false);
+        assert_eq!(cpu.n, true);
+        assert_eq!(cpu.h, true);
+        assert_eq!(cpu.cy, true);
+        assert_eq!(cpu.rf[ArithmeticOperand::A as usize], 0xff);
+    }
+}
+
+// Verify subtracting registers with carry
+#[test]
+fn test_sub_carry() {
+    let mut cpu = Cpu::new();
+    const INSTRUCTIONS_LEN: usize = 6;
+    let test_ram: [u8; INSTRUCTIONS_LEN] = [
+        Instruction::Sbc(ArithmeticOperand::B).as_byte(),
+        Instruction::Sbc(ArithmeticOperand::C).as_byte(),
+        Instruction::Sbc(ArithmeticOperand::D).as_byte(),
+        Instruction::Sbc(ArithmeticOperand::E).as_byte(),
+        Instruction::Sbc(ArithmeticOperand::H).as_byte(),
+        Instruction::Sbc(ArithmeticOperand::L).as_byte(),
+    ];
+
+    cpu.load_test_ram(&test_ram);
+    cpu.rf.iter_mut().for_each(|x| *x = 1);
+
+    for i in 0..test_ram.len() {
+        cpu.rf[ArithmeticOperand::A as usize] = 2;
+        cpu.cy = true;
+        cpu.step();
+
+        assert_eq!(cpu.z, true);
+        assert_eq!(cpu.n, true);
+        assert_eq!(cpu.h, false);
+        assert_eq!(cpu.cy, false);
+        assert_eq!(cpu.rf[ArithmeticOperand::A as usize], 0);
+    }
+}
+
+// Verify adding to registers with overflow
+#[test]
+fn test_sub_carry_overflow() {
+    let mut cpu = Cpu::new();
+    const INSTRUCTIONS_LEN: usize = 6;
+    let test_ram: [u8; INSTRUCTIONS_LEN] = [
+        Instruction::Sbc(ArithmeticOperand::B).as_byte(),
+        Instruction::Sbc(ArithmeticOperand::C).as_byte(),
+        Instruction::Sbc(ArithmeticOperand::D).as_byte(),
+        Instruction::Sbc(ArithmeticOperand::E).as_byte(),
+        Instruction::Sbc(ArithmeticOperand::H).as_byte(),
+        Instruction::Sbc(ArithmeticOperand::L).as_byte(),
+    ];
+
+    cpu.load_test_ram(&test_ram);
+    cpu.rf.iter_mut().for_each(|x| *x = 1);
+
+    for i in 0..test_ram.len() {
+        cpu.rf[ArithmeticOperand::A as usize] = 0;
+        cpu.cy = true;
+        cpu.step();
+
+        assert_eq!(cpu.z, false);
+        assert_eq!(cpu.n, true);
+        assert_eq!(cpu.h, true);
+        assert_eq!(cpu.cy, true);
+        assert_eq!(cpu.rf[ArithmeticOperand::A as usize], 0xfe);
+    }
+}
+
+// Verify subtracting immediates
+#[test]
+fn test_sub_immediate() {
+    let mut cpu = Cpu::new();
+    const INSTRUCTIONS_LEN: usize = 2;
+    let test_ram: [u8; INSTRUCTIONS_LEN] = [
+        Instruction::SubImm().as_byte(),
+        0x10,
+    ];
+    cpu.rf[ArithmeticOperand::A as usize] = 0x10;
+    cpu.load_test_ram(&test_ram);
+    cpu.step();
+    assert_eq!(cpu.rf[ArithmeticOperand::A as usize], 0);
+    assert_eq!(cpu.z, true);
+    assert_eq!(cpu.n, true);
+    assert_eq!(cpu.h, false);
+    assert_eq!(cpu.cy, false);
+}
+
+// Verify subtracting immediates with overflow
+#[test]
+fn test_sub_immediate_overflow() {
+    let mut cpu = Cpu::new();
+    const INSTRUCTIONS_LEN: usize = 2;
+    let test_ram: [u8; INSTRUCTIONS_LEN] = [
+        Instruction::SubImm().as_byte(),
+        0x01,
+    ];
+    cpu.load_test_ram(&test_ram);
+    cpu.rf[ArithmeticOperand::A as usize] = 0;
+    cpu.step();
+    assert_eq!(cpu.rf[ArithmeticOperand::A as usize], 0xff);
+    assert_eq!(cpu.z, false);
+    assert_eq!(cpu.n, true);
+    assert_eq!(cpu.h, true);
+    assert_eq!(cpu.cy, true);
+}
+
