21bc4d845a33a615cc843b5dc27b2a07c7ed58b6
Nick Elsmore <nicholaselsmore@gmail.com>
2021-11-02 13:59:13 -0700

    joypad: add test module

---
+++ src/joypad/test.rs
@@ -0,0 +1,41 @@
+// Test module for Joypad.
+//
+// Provides unit tests to test basic functionality
+// of the Joypad module.
+
+use super::*;
+
+const P1: u16 = 0xff00;
+
+// Verify register properly reflects state
+#[test]
+fn test_joypad_reg() {
+    let mut jp = Joypad::new();
+
+    let reg = jp.mem_read_byte(P1);
+    assert_eq!(reg, 0x3f,
+               "default: reported {:#04x} instead of {:#04x}", reg, 0x3f);
+
+    jp.up = true;
+    jp.down = true;
+    jp.left = true;
+    jp.right = true;
+    jp.direction_select = true;
+    let reg = jp.mem_read_byte(P1);
+    assert_eq!(reg, 0x20,
+               "direction: reported {:#04x} instead of {:#04x}", reg, 0x20);
+
+    jp.direction_select = false;
+    jp.action_select = true;
+    let reg = jp.mem_read_byte(P1);
+    assert_eq!(reg, 0x1f,
+               "action: reported {:#04x} instead of {:#04x}", reg, 0x1f);
+
+    jp.a = true;
+    jp.b = true;
+    jp.select = true;
+    jp.start = true;
+    let reg = jp.mem_read_byte(P1);
+    assert_eq!(reg, 0x10,
+               "action: reported {:#04x} instead of {:#04x}", reg, 0x10);
+}
