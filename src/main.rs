4796c9055aece8ffec6dfe127391aef55e335970
Nick Elsmore <nicholaselsmore@gmail.com>
2022-04-19 18:16:53 -0700

    load rom file from argument

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,35 @@ mod joypad;
 mod memory;
 mod mmu;
 
+use std::env;
+use std::io;
+
 use crate::gameboy::Gameboy;
 
 const WIDTH: u32 = 800;
 const HEIGHT: u32 = 600;
 
-fn main() -> Result<(), String> {
-    let mut gameboy = Gameboy::init(WIDTH, HEIGHT)?;
-    gameboy.run()?;
+fn print_usage() {
+    println!("usage: dookieboy rom_path");
+    println!("  rom_path: absolute or relative path to ROM file");
+}
 
-    Ok(())
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        print_usage();
+        std::process::exit(1);
+    }
+
+    let rom = String::from(&args[1]);
+
+    let mut gameboy = Gameboy::init(WIDTH, HEIGHT).unwrap();
+    match gameboy.run(rom) {
+        Ok(v) => {},
+        Err(e) => {
+            println!("dookieboy encountered big error: {}\n", e);
+            print_usage();
+            std::process::exit(1);
+        },
+    }
 }
