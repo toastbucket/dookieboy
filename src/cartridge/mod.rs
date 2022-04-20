c62651f5b0218ba2aaed50d85a744d1c99ed1d15
Nick Elsmore <nicholaselsmore@gmail.com>
2022-04-19 18:06:04 -0700

    cartridge: fully parse header

--- src/cartridge/mod.rs
+++ src/cartridge/mod.rs
@@ -6,6 +6,9 @@ use std::io;
 
 use crate::memory::Memory;
 
+const HEADER_START: usize = 0x100;
+const HEADER_SIZE: usize = 80;
+
 #[derive(Debug)]
 enum CartridgeType {
     RomOnly = 0x00,
@@ -49,6 +52,8 @@ struct Header {
     title: String,
     cart_type: CartridgeType,
     cart_mode: CartridgeMode,
+    rom_size: usize,
+    ram_size: usize,
     header_checksum: u8,
     cart_checksum: u16,
 }
@@ -59,9 +64,19 @@ impl Header {
             title: String::new(),
             cart_type: CartridgeType::RomOnly,
             cart_mode: CartridgeMode::PgbMode,
+            rom_size: 0,
+            ram_size: 0,
             header_checksum: 0,
             cart_checksum: 0,
         }
+    }
+
+    fn dump(&self) {
+        println!("Title: {}", self.title);
+        println!("Type: {:?}", self.cart_type);
+        println!("Mode: {:?}", self.cart_mode);
+        println!("ROM: {}KiB", self.rom_size / 1024);
+        println!("RAM: {}KiB", self.ram_size / 1024);
     }
 }
 
@@ -80,11 +95,17 @@ impl Cartridge {
         }
     }
 
-    pub fn load_rom(&mut self, path: String) -> Result<(),io::Error> {
+    pub fn load_rom(&mut self, path: String) -> Result<(), io::Error> {
         self.rom = fs::read(path)?;
-        self.loaded = true;
+        if self.rom.len() < (HEADER_START + HEADER_SIZE) {
+            panic!("missing header");
+        }
 
         self.parse_header();
+        self.loaded = true;
+
+        #[cfg(debug_assertions)]
+        self.header.dump();
 
         Ok(())
     }
@@ -111,6 +132,70 @@ impl Cartridge {
             self.header.cart_mode = CartridgeMode::CgbSupported;
         } else {
             self.header.cart_mode = CartridgeMode::PgbMode;
+        }
+
+       self.header.cart_type = match self.mem_read_byte(0x147) {
+            0x00 => CartridgeType::RomOnly,
+            0x01 => CartridgeType::Mbc1,
+            0x02 => CartridgeType::Mbc1Ram,
+            0x03 => CartridgeType::Mbc1RamBattery,
+            0x05 => CartridgeType::Mbc2,
+            0x06 => CartridgeType::Mbc2Battery,
+            0x08 => CartridgeType::RomRam,
+            0x09 => CartridgeType::RomRamBattery,
+            0x0b => CartridgeType::Mmm01,
+            0x0c => CartridgeType::Mmm01Ram,
+            0x0d => CartridgeType::Mmm01RamBattery,
+            0x0f => CartridgeType::Mbc3TimerBattery,
+            0x10 => CartridgeType::Mbc3TimerRamBattery,
+            0x11 => CartridgeType::Mbc3,
+            0x12 => CartridgeType::Mbc3Ram,
+            0x13 => CartridgeType::Mbc3RamBattery,
+            0x19 => CartridgeType::Mbc5,
+            0x1a => CartridgeType::Mbc5Ram,
+            0x1b => CartridgeType::Mbc5RamBattery,
+            0x1c => CartridgeType::Mbc5Rumble,
+            0x1d => CartridgeType::Mbc5RumbleRam,
+            0x1e => CartridgeType::Mbc5RumbleRamBattery,
+            0x20 => CartridgeType::Mbc6,
+            0x22 => CartridgeType::Mbc7SensorRumbleRamBattery,
+            0xfc => CartridgeType::PocketCamera,
+            0xfd => CartridgeType::BandaiTama5,
+            0xfe => CartridgeType::HuC3,
+            0xff => CartridgeType::HuC1RamBattery,
+            _ => panic!("invalid cartridge type"),
+        };
+
+        let rom_size_code = self.mem_read_byte(0x148);
+
+        /*
+         * only support 0x00-0x08
+         * https://gbdev.io/pandocs/The_Cartridge_Header.html#0148---rom-size
+         *
+         * "[0x52-54 are] only listed in unofficial docs. No cartridges or ROM files using these sizes are known.
+         * As the other ROM sizes are all powers of 2, these are likely inaccurate. The source for
+         * these values is unknown."
+         */
+        if rom_size_code > 0x08 {
+            panic!("invalid ROM size");
+        }
+        self.header.rom_size = 32768 << rom_size_code;
+
+        self.header.ram_size = match self.mem_read_byte(0x149) {
+            0x00 => 0, // no RAM
+            0x02 => 8192, // 8KiB
+            0x03 => 32768, // 32KiB
+            0x04 => 131072, // 128KiB
+            0x05 => 65536, // 64KiB
+            _ => panic!("invalid RAM size"),
+        };
+
+        // When using a MBC2 chip, 0x00 must be specified as the RAM Size, even though the MBC2
+        // includes a built-in RAM of 512 x 4 bits.
+        if (matches!(self.header.cart_type, CartridgeType::Mbc2)
+            || matches!(self.header.cart_type, CartridgeType::Mbc2Battery))
+           && self.header.ram_size != 0 {
+            panic!("RAM size must be 0 when using MBC2 chip");
         }
     }
 
