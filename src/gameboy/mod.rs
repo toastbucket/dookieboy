2a5aeea1ce39521c49f2ad0fe340a345a8369c83
Nick Elsmore <nicholaselsmore@gmail.com>
2022-05-06 19:17:42 -0700

    gameboy: rerun last command if input is empty
    WIP: more shit

--- src/gameboy/mod.rs
+++ src/gameboy/mod.rs
@@ -17,6 +17,66 @@ use crate::mmu::Mmu;
 use crate::joypad::Joypad;
 use crate::memory::Memory;
 
+#[derive(Debug)]
+struct Cmd {
+    cmd: String,
+    args: Vec<String>,
+}
+
+impl Cmd {
+    fn new(raw: String) -> Cmd {
+        let mut cmd: String = String::new();
+        let mut args: Vec<String> = Vec::new();
+        for (idx, arg) in raw.split(" ").enumerate() {
+            if idx == 0 {
+                cmd = String::from(arg);
+            } else {
+                args.push(String::from(arg));
+            }
+        }
+
+        Cmd {
+            cmd: cmd,
+            args: args,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Cli {
+    cmd: Option<Cmd>,
+    last_cmd: Option<Cmd>,
+}
+
+impl Cli {
+    fn new() -> Cli {
+        Cli {
+            cmd: None,
+            last_cmd: None,
+        }
+    }
+
+    fn show_cli_prompt() {
+        print!("dookie>");
+        io::stdout().flush().expect("couldn't flush??");
+    }
+
+    fn get_cmd(&mut self) {
+        let input = io::stdin()
+            .lock()
+            .lines()
+            .next()
+            .unwrap_or_else(|| Ok(String::new()))
+            .expect("uh oh");
+
+        self.cmd = Some(Cmd::new(input));
+    }
+
+    fn save_last(&mut self) {
+        self.last_cmd = self.cmd.take();
+    }
+}
+
 pub struct Gameboy {
     cpu: Cpu,
     mmu: Rc<RefCell<Mmu>>,
@@ -46,30 +106,64 @@ impl Gameboy {
         })
     }
 
+
+    fn run_cmd(&mut self, cmd: &Option<Cmd>) -> bool {
+        match cmd {
+            Some(cmd) => {
+                match cmd.cmd.as_ref() {
+                    "step" => {
+                        let mut num_steps = 1;
+                        if cmd.args.len() > 0 {
+                            num_steps = cmd.args[0].parse::<u32>().unwrap_or(1);
+                        }
+                        for _ in 0..num_steps {
+                            self.cpu.step();
+                        }
+
+                        true
+                    },
+                    "" => {
+                        false
+                    },
+                    "q" | "e" | "quit" | "exit" => {
+                        std::process::exit(0);
+                        true
+                    },
+                    _ => {
+                        println!("invalid command!");
+                        true
+                    },
+                }
+            },
+            None => {
+                println!("no command :(");
+                false
+            },
+        }
+    }
+
     pub fn run(&mut self, rom: String, debug: bool) -> Result<(), io::Error> {
 
         self.mmu.borrow_mut().cartridge.load_rom(rom)?;
         self.cpu.reset();
 
-        'running: loop {
-            self.handle_sdl2_events()?;
+        if debug {
+            let mut cli = Cli::new();
 
-            if debug {
-                print!("dookie>");
-                io::stdout().flush().expect("couldn't flush??");
-                let cmd = io::stdin().lock().lines().next().unwrap();
-                match cmd.expect("uh oh").as_ref() {
-                    "step" => {
-                        self.cpu.step();
-                    },
-                    "q" | "e" | "quit" | "exit" => {
-                        std::process::exit(0);
-                    },
-                    _ => {
-                        println!("invalid command!");
-                    },
-                }
-            } else {
+            'debug: loop {
+                    Cli::show_cli_prompt();
+                    cli.get_cmd();
+
+                    let ret = self.run_cmd(&cli.cmd);
+                    if ret {
+                        cli.save_last();
+                    } else {
+                        self.run_cmd(&cli.last_cmd);
+                    }
+            }
+        } else {
+            'running: loop {
+                self.handle_sdl2_events()?;
                 self.cpu.step();
             }
         }
