f8fc2e649839f07dfceb6c55098a0c37b171e282
Nick Elsmore <nicholaselsmore@gmail.com>
2022-05-09 14:33:55 -0700

    launch debug shell with debug flag cli argument

--- src/gameboy/mod.rs
+++ src/gameboy/mod.rs
@@ -11,6 +11,7 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::render::WindowCanvas;
 
+use crate::shell::{Shell, Cmd};
 use crate::cpu::Cpu;
 use crate::mmu::Mmu;
 use crate::joypad::Joypad;
@@ -45,15 +46,60 @@ impl Gameboy {
         })
     }
 
+    fn run_cmd(&mut self, cmd: &Option<Cmd>) -> bool {
+        match cmd {
+            Some(cmd) => {
+                match cmd.cmd.as_ref() {
+                    "step" => {
+                        let mut num_steps = 1;
+                        if cmd.args.len() > 0 {
+                            num_steps = cmd.args[0].parse::<usize>().unwrap_or(1);
+                        }
 
-    pub fn run(&mut self, rom: String) -> Result<(), io::Error> {
+                        for _ in 0..num_steps {
+                            self.cpu.step();
+                        }
+                    },
+                    "dump" => {
+                        println!("CPU:\n{}", self.cpu);
+                    },
+                    "q" | "e" | "quit" | "exit" => {
+                        std::process::exit(0);
+                    },
+                    _ => {
+                        println!("invalid command: {}", cmd.cmd);
+                    },
+                }
+
+                true
+            },
+            None => false
+        }
+    }
+
+    pub fn run(&mut self, rom: String, debug: bool) -> Result<(), io::Error> {
 
         self.mmu.borrow_mut().cartridge.load_rom(rom)?;
         self.cpu.reset();
 
-        'running: loop {
-            self.handle_sdl2_events()?;
-            sleep(Duration::from_millis(100));
+        if debug {
+            let mut last_cmd: Option<Cmd> = None;
+            let mut cmd: Option<Cmd> = None;
+
+            'debug: loop {
+                cmd = Shell::get_cmd();
+                let ret = self.run_cmd(&cmd);
+                if !ret {
+                    self.run_cmd(&last_cmd);
+                } else {
+                    last_cmd = cmd.take();
+                }
+            }
+        } else {
+            'running: loop {
+                self.handle_sdl2_events()?;
+                sleep(Duration::from_millis(100));
+            }
         }
 
         Ok(())
--- src/main.rs
+++ src/main.rs
@@ -14,22 +14,57 @@ use crate::gameboy::Gameboy;
 const WIDTH: u32 = 800;
 const HEIGHT: u32 = 600;
 
+fn is_gb_rom(filename: &str) -> bool {
+    if filename.len() > 3 {
+        if &filename[filename.len()-3..filename.len()] == ".gb" {
+            return true;
+        }
+    }
+    false
+}
+
 fn print_usage() {
-    println!("usage: dookieboy rom_path");
+    println!("usage: dookieboy [-d] rom_path");
     println!("  rom_path: absolute or relative path to ROM file");
+    println!("  -d:       enable debug shell");
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
+    let num_args = args.len();
+    if num_args < 2 {
         print_usage();
         std::process::exit(1);
     }
 
-    let rom = String::from(&args[1]);
+    // argument fields
+    let mut debug: bool = false;
+    let mut rom: String = String::new();
+
+    for (i, arg) in args.iter().enumerate() {
+        if i == 0 {
+            continue;
+        }
+
+        if i == (num_args - 1) {
+            if !is_gb_rom(arg.as_str()) {
+                println!("valid rom path not provided");
+                print_usage();
+                std::process::exit(1);
+            }
+
+            rom = String::from(arg.as_str());
+        }
+
+        match arg.as_str() {
+            #[cfg(debug_assertions)]
+            "-d" => debug = true,
+            &_ => {},
+        }
+    }
 
     let mut gameboy = Gameboy::init(WIDTH, HEIGHT).unwrap();
-    match gameboy.run(rom) {
+    match gameboy.run(rom, debug) {
         Ok(v) => {},
         Err(e) => {
             println!("dookieboy encountered big error: {}\n", e);
